bfaa7b8914adccc80833531f8af4a8a6187fa218
Moroni Motta <dareevil7@gmail.com>
2024-07-25 11:43:34 -0300

    account_form

--- server/index.js
+++ server/index.js
@@ -4,12 +4,19 @@ const app = express();
 const cors = require("cors");
 const fs = require("fs");
 const path = require("path");
+const bodyParser = require('body-parser');
+
 
 const server = http.createServer(app);
 const port = process.env.PORT || 3000;
 require("dotenv").config();
 
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true })); 
+
 app.use(express.static(path.join(__dirname, '../src/public')));
+app.use(cors());
+
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../src/index.html"));
@@ -38,5 +45,22 @@ app.post('/movies/list/add', (req, res) => {
     );
 });
 
+
+app.get('/account/create', (req, res) => {
+    res.sendFile(path.join(__dirname, "../src/account/create.html"));
+});
+
+app.post('/account/create', (req, res) => {
+    console.log('Received data:', req.body);
+  
+    fs.writeFile(path.join(__dirname, '../src/public/json/users.json'), JSON.stringify(req.body), (err) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Failed to save data.' });
+      } else {
+        res.status(200).json({ message: 'Account created successfully!' });
+      }
+    });
+  });
 
 server.listen(port, () => console.log(`Server running on port ${port}`));
