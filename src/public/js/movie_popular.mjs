0a16983c2aa830d2fadd625cc32ef53300d8a0ed
Moroni Motta <dareevil7@gmail.com>
2024-07-25 23:18:17 -0300

    mjs

--- src/public/js/movie_popular.mjs
+++ src/public/js/movie_popular.mjs
@@ -1,12 +1,9 @@
-import { getRandomQuote, makeRequest } from './utils.mjs';
+import { makeRequest } from './utils.mjs';
 import MovieCard from './movieCard.mjs';
 
-document.addEventListener("DOMContentLoaded", async () => {
-    getRandomQuote();
-    const movies = await makeRequest('movie/popular');
-    movies.results.forEach(movie => {
-        const movieCard = new MovieCard(movie);
-        const card = movieCard.renderMovieCard();
-        document.getElementById('movie-container').appendChild(card);
-    });
+const movies = await makeRequest('movie/popular');
+movies.results.forEach(movie => {
+    const movieCard = new MovieCard(movie);
+    const card = movieCard.renderMovieCard();
+    document.getElementById('movie-container').appendChild(card);
 });
