41dd2b98362eec2872c02740d9b7db48d76e5359
Moroni Motta <dareevil7@gmail.com>
2024-07-25 23:21:53 -0300

    test js

--- src/public/js/movie_popular.mjs
+++ src/public/js/movie_popular.mjs
@@ -1,9 +1,14 @@
+console.log('movie_popular.mjs loaded');
+
 import { makeRequest } from './utils.mjs';
 import MovieCard from './movieCard.mjs';
 
-const movies = await makeRequest('movie/popular');
-movies.results.forEach(movie => {
+(async () => {
+  const movies = await makeRequest('movie/popular');
+  console.log('Movies fetched:', movies); // Debugging
+  movies.results.forEach(movie => {
     const movieCard = new MovieCard(movie);
     const card = movieCard.renderMovieCard();
     document.getElementById('movie-container').appendChild(card);
-});
+  });
+})();
